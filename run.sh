39550d6028f19f226598c57ab1635cd73b643321
Bill Gestrich <bill@foreflight.com>
2020-08-21 10:18:01 -0400

    Working decently

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ set -u
 TAG_NAME="pi-container"
 PI_PROJECT_DIR="/home/pi"
 EXECUTABLE_NAME="pi"
+buildDirName=".build_linux"
 
 #Shell into docker
 #docker run --rm -ti --volume "$(pwd)/:/src"  --workdir "/src/" bill-swift-hello-world:latest /bin/bash
@@ -20,10 +21,13 @@ function buildDockerImage(){
 #Had to run ssh-add
 function deploy(){
   echo "STAGE: deploy start"
-  docker stop $TAG_NAME
-  docker rm $TAG_NAME
+  #docker stop $TAG_NAME
+  #docker rm $TAG_NAME
   #docker run --name $TAG_NAME --volume "$(pwd)/:/src"  --workdir "/src/" bill-swift-hello-world:latest ./run.sh buildSwift 
-  docker run --name $TAG_NAME --volume "$(pwd)/:/app/src"  --workdir "/app/src/" bill-swift-hello-world:latest ./run.sh buildSwift 
+  #docker run --name $TAG_NAME --volume "$(pwd)/:/app/src"  --workdir "/app/src/" bill-swift-hello-world:latest ./run.sh buildSwift 
+  #docker run --volume "$(pwd)/:/app/src"  --workdir "/app/src/" bill-swift-hello-world:latest ./run.sh buildSwift 
+  docker run --rm --volume "$(pwd)/:/src"  --workdir "/src/" bill-swift-hello-world:latest ./run.sh buildSwift
+  stopPi
   pushToPi
   runPi
   echo "STAGE: deploy done"
@@ -35,15 +39,23 @@ function redeploy(){
   docker start $TAG_NAME --attach
 }
 
+function stopPi(){
+  echo "STAGE: Stopping Pi"
+  ssh pi@192.168.1.217 "$PI_PROJECT_DIR/run.sh pi_stop" || 
+  echo "STAGE: Stopping Pi done"
+}
+
 function pushToPi(){
   echo "STAGE: Push to Pi"
-  scp -i ~/.ssh/id_rsa raspberry-pi pi@192.168.1.217:$PI_PROJECT_DIR/$EXECUTABLE_NAME
+  scp -i ~/.ssh/id_rsa $buildDirName/debug/raspberry-pi pi@192.168.1.217:$PI_PROJECT_DIR/$EXECUTABLE_NAME
   scp -i ~/.ssh/id_rsa run.sh pi@192.168.1.217:$PI_PROJECT_DIR/run.sh
   echo "STAGE: Push to Pi done"
 }
 
 function runPi(){
-  ssh pi@192.168.1.217 "$PI_PROJECT_DIR/run.sh pi_run"
+  echo "STAGE: Running Pi"
+  ssh -tt pi@192.168.1.217 "$PI_PROJECT_DIR/run.sh pi_run"
+  echo "STAGE: Running Pi done"
 }  
 
 function pi_run(){
@@ -52,29 +64,29 @@ function pi_run(){
 
 function pi_stop(){
   pkill -f -x $EXECUTABLE_NAME ||
+  sleep 1 
 }  
 
 function buildSwift(){
 
-  originalDir="$(pwd)"
-  stagingDir="/test"
-  buildDirName=".build_linux"
-  buildDir="$stagingDir/$buildDirName"
-  echo ".build_linux contents: $(ls .build_linux)"
-  rm -rf $stagingDir || echo "No staging directory to delete"
-  mkdir $stagingDir || echo "Container exists"
-  echo "About to copy $stagingDir"
-  cp -r * $stagingDir
-  cd $stagingDir
+  #originalDir="$(pwd)"
+  #stagingDir="/test"
+  #buildDir="$stagingDir/$buildDirName"
+  #echo ".build_linux contents: $(ls .build_linux)"
+  #rm -rf $stagingDir || echo "No staging directory to delete"
+  #mkdir $stagingDir || echo "Container exists"
+  #echo "About to copy $stagingDir"
+  #cp -r * $stagingDir
+  #cd $stagingDir
 
-  mkdir $buildDir || echo "Couldn't create $buildDir. Maybe already exists"
-  swift build --build-path $buildDir
-  executablePath="$buildDir/debug/raspberry-pi"
-  echo "About to copy $executablePath"
-  cp $executablePath $originalDir
-  echo "About to copy $buildDir"
-  rm -rf $originalDir/$buildDirName 
-  cp -r $buildDir $originalDir
+  #mkdir $buildDir || echo "Couldn't create $buildDir. Maybe already exists"
+  swift build --build-path $buildDirName
+  executablePath="$buildDirName/debug/raspberry-pi"
+  #echo "About to copy $executablePath"
+  #cp $executablePath $originalDir
+  #echo "About to copy $buildDir"
+  #rm -rf $originalDir/$buildDirName 
+  #cp -r $buildDir $originalDir
 }
 
 
